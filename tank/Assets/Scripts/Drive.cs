f1eb5412be7a9055b0992bd245a2d41186662146
chuiizeet <chuy.guerra19@outlook.es>
2019-09-30 21:00:17 -0500

    DRY vectors

--- tank/Assets/Scripts/Drive.cs
+++ tank/Assets/Scripts/Drive.cs
@@ -4,26 +4,33 @@ using System.Collections;
 // A very simplistic car driving on the x-z plane.
 
 public class Drive : MonoBehaviour
-{
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
+{   
+    Vector2 up = new Vector2(0, 0.1f);
+    Vector2 left = new Vector2(-0.1f, 0);
 
     void Update()
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(0, translation, 0);
-
-        // Rotate around our y-axis
-        transform.Rotate(0, 0, -rotation);
+        Vector3 position = this.transform.position;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            position.x += up.x;
+            position.y += up.y;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            position.x += -up.x;
+            position.y += -up.y;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x += left.x;
+            position.y += left.y;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += -left.x;
+            position.y += -left.y;
+        }
+        this.transform.position = position;
     }
 }
